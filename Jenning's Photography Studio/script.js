2047d18f87b39e2aea47cae91258eedb9d6d2b3e
Christina Meador <christinameador1@gmail.com>
2020-05-14 06:44:24 -0500

    Added Jenning's Photography Studio Website
    Photography Concept Website

---
+++ Jenning's Photography Studio/script.js
@@ -0,0 +1,81 @@
+(function($) { "use strict";
+		
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+
+	
+	//About page
+	
+	$(".about-text").on('click', function () {
+		$("body").addClass("about-on");
+	});
+	$(".about-close").on('click', function () {
+		$("body").removeClass("about-on");
+	});
+
+	
+	//Contact page
+	
+	$(".contact-text").on('click', function () {
+		$("body").addClass("contact-on");
+	});
+	$(".contact-close").on('click', function () {
+		$("body").removeClass("contact-on");
+	});
+
+	
+	//weddings portfolio page
+	
+	$(".weddings").on('click', function () {
+		$("body").addClass("weddings-on");
+	});
+	$(".weddings-close").on('click', function () {
+		$("body").removeClass("weddings-on");
+	});
+
+	
+	//engagements portfolio page
+	
+	$(".engagements").on('click', function () {
+		$("body").addClass("engagements-on");
+	});
+	$(".engagements-close").on('click', function () {
+		$("body").removeClass("engagements-on");
+	});
+
+	
+	//seniors portfolio page
+	
+	$(".seniors").on('click', function () {
+		$("body").addClass("seniors-on");
+	});
+	$(".seniors-close").on('click', function () {
+		$("body").removeClass("seniors-on");
+	});
+
+	
+})(jQuery);
